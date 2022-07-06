b0e146fc3d10fb283abda90008383ba0db208667
chan-00 <a82316@naver.com>
2022-07-06 13:43:37 +0900

    2022.07.06일자 공부 기록입니다.

---
+++ 2022_07_StudyNote/220706_StudyNote/travelPageJS.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  var td = document.getElementById('today');
+  var cd = document.getElementById('changeDay');
+  td.innerHTML = "현재 날짜 : " + Date();
+  cd.innerHTML = "마지막 수정 날짜 : 2022.07.03 19:30";
+}
